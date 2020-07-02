877503defc1ab62324b469290ae2dd25574d9737
swapnil-2 <67337605+swapnil-2@users.noreply.github.com>
2020-07-02 13:57:00 -0400

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "shell_array.h"
+#include "shell_list.h"
+
+int main(int argc, char * argv[]){
+  //Write your solution here
+  
+
+
+  return EXIT_SUCCESS;
+}
